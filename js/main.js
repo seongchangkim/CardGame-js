05b42772b9f8be81136ac0e1440f6a050f594f29
김성창 <dark21@gmail.com>
2022-02-07 11:27:40 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,57 @@
+const pageContent = document.querySelector('.page-content');
+const number = Array(9).fill(" ");
+let choiseLife = 3;
+let choisedCardNumbers = [];
+let count = 1;
+
+const choise = (event) => {
+    choiseLife--;
+    
+    const target = event.target;
+    const num = target.lastElementChild;
+
+    target.style.background = 'red';
+
+    let nanNum = Math.floor(Math.random()*3)+1;
+    num.innerText = nanNum;
+    choisedCardNumbers.push(nanNum);
+
+    let index = choisedCardNumbers.length-2;
+
+    if(choisedCardNumbers.length > 1){
+        for(let i= index; i < choisedCardNumbers.length-1; i++){
+            for(let j =index+1; j<choisedCardNumbers.length; j++){
+                if(choisedCardNumbers[i] === choisedCardNumbers[j]){
+                    count++;
+                }
+            }
+        }
+    }
+    
+    if(choisedCardNumbers.length === 3 && count === 3){
+        alert(`You're victory`);
+        setTimeout(() => location.reload(true), 1000);
+    }else if(choiseLife <= 0 && count !== 3){
+        alert('Game Over!!');
+        setTimeout(() => location.reload(true), 1000);
+    }
+}
+
+number.forEach(() =>{
+    pageContent.innerHTML += `
+        <div class='card'>
+            <span class="num"></span>
+        </div>
+    `; 
+    
+    const card = document.querySelectorAll('.card');
+    
+    if(card !== []){
+        for(let car of card){
+            car.addEventListener('click', choise);
+        }
+    }
+});
+
+
+
